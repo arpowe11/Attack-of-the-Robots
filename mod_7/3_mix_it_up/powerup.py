0dd3809fb5f510d0a43b5aa6ba4bb8d3ae81508f
Alexander R. Powell <161773628+arpowe11@users.noreply.github.com>
2024-07-02 13:25:39 -0400

    Add files via upload

---
+++ mod_7/3_mix_it_up/powerup.py
@@ -0,0 +1,39 @@
+import pygame
+import random
+import toolbox
+
+
+class PowerUp(pygame.sprite.Sprite):
+    def __init__(self, screen, x, y) -> None:
+        pygame.sprite.Sprite.__init__(self, self.containers)  # NOQA
+
+        # Set up PowerUp variables
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.pick_power = random.randint(0, 1)
+
+        if self.pick_power == 0:
+            self.image = pygame.image.load("../assets/powerupCrate.png")
+            self.background_image = pygame.image.load("../assets/powerupBackgroundBlue.png")
+            self.power_type = "crateammo"  # NOQA
+        elif self.pick_power == 1:
+            self.image = pygame.image.load("../assets/powerupExplosiveBarrel.png")
+            self.background_image = pygame.image.load("../assets/powerupBackgroundRed.png")
+            self.power_type = "explosiveammo"  # NOQA
+
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.x, self.y)
+        self.background_angle = 0
+        self.spinny_speed = 2
+
+    def update(self, player) -> None:
+        if self.rect.colliderect(player.rect):
+            player.power_up(self.power_type)
+            self.kill()
+
+        self.background_angle += self.spinny_speed
+        bg_image_to_draw, bg_rect = toolbox.get_rotated_image(self.background_image, self.rect, self.background_angle)
+
+        self.screen.blit(bg_image_to_draw, bg_rect)
+        self.screen.blit(self.image, self.rect)
